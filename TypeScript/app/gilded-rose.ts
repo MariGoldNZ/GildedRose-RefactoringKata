7e3b7500b6809145ccec1bc3934361e6fcc09cfd
MariGoldNZ <m.goldberg.nz@gmail.com>
2023-12-13 13:12:16 +0100

    Enums for Item Names
    Implementing different strategies (ConjuredStrategy, AgedBrieStrategy, BackstagePassesStrategy, and DefaultStrategy) for different types of items.
    
    Each strategy class implements the UpdateStrategy interface and contains specific logic for updating its respective item type.
    
    Now uses a map of strategies to handle item updates. The updateQuality method determines which strategy to use based on the item's name. If an item doesn't match any specific type, it falls back to the DefaultStrategy.
    Added method getStrategy to check for partial strings in ItemName

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,3 +1,10 @@
+enum ItemName {
+  Sulfuras = 'Sulfuras, Hand of Ragnaros',
+  AgedBrie = 'Aged Brie',
+  BackstagePasses = 'Backstage passes',
+  Conjured = 'Conjured'
+  }
+
 export class Item {
   name: string;
   sellIn: number;
@@ -10,74 +17,115 @@ export class Item {
   }
 }
 
-export class GildedRose {
-  private static readonly MAX_QUALITY = 50;
-  private static readonly MIN_QUALITY = 0;
-  private static readonly SULFURAS = 'Sulfuras, Hand of Ragnaros';
-  private static readonly AGED_BRIE = 'Aged Brie';
-  private static readonly BACKSTAGE_PASSES = 'Backstage passes';
+interface UpdateStrategy {
+  update(item: Item): void;
+}
 
+const MAX_QUALITY = 50;
+const MIN_QUALITY = 0;
+const QUALITY_DEGRADE = 1;
+const QUALITY_UPGRADE = 1;
+const SELLIN_THRESHOLD_TEN_DAYS = 10;
+const SELLIN_THRESHOLD_FIVE_DAYS = 5;
+const SELLIN_DECREMENT = 1;
+const CONJURED_DEGRADE_MULTIPLIER = 2;
+const SELLIN_QUALITY_THRESHOLD = 0;
 
-  items: Array<Item>;
-
-  constructor(items = [] as Array<Item>) {
-    this.items = items;
-  }
-
-  private updateConjuredItem(item: Item): void{
-    item.sellIn = item.sellIn - 1;
-    item.quality = Math.max(0, item.quality - 2);
-    if (item.sellIn < 0) {
-      item.quality = Math.max(0, item.quality - 2);
+class ConjuredStrategy implements UpdateStrategy {
+  update(item: Item): void {
+    item.sellIn -= SELLIN_DECREMENT;
+    item.quality = Math.max(MIN_QUALITY, item.quality - QUALITY_DEGRADE * CONJURED_DEGRADE_MULTIPLIER);
+    if (item.sellIn < SELLIN_QUALITY_THRESHOLD) {
+      item.quality = Math.max(MIN_QUALITY, item.quality - QUALITY_DEGRADE * CONJURED_DEGRADE_MULTIPLIER);
     }
-    
+  }
+}
+class AgedBrieStrategy implements UpdateStrategy {
+  update(item: Item): void {
+    item.sellIn -= SELLIN_DECREMENT;
+    if (item.quality < MAX_QUALITY) {
+      item.quality += QUALITY_UPGRADE;
+    }
+  }
+}
+
+class LegendaryStrategy implements UpdateStrategy {
+  update(item: Item): void {
+    item.quality = 80;
+  }
+}
+
+class BackstagePassesStrategy implements UpdateStrategy {
+  update(item: Item): void {
+    item.sellIn -= SELLIN_DECREMENT;
+    if (item.sellIn < MIN_QUALITY) {
+      item.quality = MIN_QUALITY;
+      return;
+    }
+    if (item.quality < MAX_QUALITY) {
+      item.quality += QUALITY_UPGRADE;
+      if (item.sellIn < SELLIN_THRESHOLD_TEN_DAYS) {
+        item.quality += QUALITY_UPGRADE;
+      }
+      if (item.sellIn < SELLIN_THRESHOLD_FIVE_DAYS) {
+        item.quality += QUALITY_UPGRADE;
+      }
+    }
+    if (item.quality > MAX_QUALITY) {
+      item.quality = MAX_QUALITY;
+    }
+  }
+}
+  
+  
+class DefaultStrategy implements UpdateStrategy {
+  update(item: Item): void {
+    item.sellIn -= SELLIN_DECREMENT;
+    if (item.quality > MIN_QUALITY) {
+      item.quality -= QUALITY_DEGRADE;
+      if (item.sellIn < MIN_QUALITY) {
+        item.quality -= QUALITY_DEGRADE;
+      }
+    }
+  }
+}
+
+export class GildedRose {
+  private strategies: Map<ItemName | 'DEFAULT', UpdateStrategy>;
+
+  constructor(public items: Array<Item> = []) {
+    this.strategies = new Map<ItemName | 'DEFAULT', UpdateStrategy>([
+      [ItemName.Conjured, new ConjuredStrategy()],
+      [ItemName.AgedBrie, new AgedBrieStrategy()],
+      [ItemName.BackstagePasses, new BackstagePassesStrategy()],
+      [ItemName.Sulfuras, new LegendaryStrategy()],
+      ['DEFAULT', new DefaultStrategy()] // Default strategy for unhandled items
+    ]);
   }
 
-  private checkName(item: Item, name: string): boolean {
-    return item.name.toLowerCase().includes(name.toLowerCase());
+  private getStrategy(itemName: ItemName): UpdateStrategy | undefined {
+    let foundStrategy: UpdateStrategy | undefined;
+    this.strategies.forEach((strategy, strategyItem) => {
+      if (
+        itemName.toLowerCase().includes(strategyItem.toLowerCase())
+      ) {
+        foundStrategy = strategy;
+      }
+    });
+    return foundStrategy || this.strategies.get('DEFAULT');
   }
 
   updateQuality(): Array<Item> {
-    this.items.forEach(item => this.updateItem(item));
+    this.items.forEach(item => {
+      const strategy = this.getStrategy(item.name as ItemName);
+      if (strategy) {
+        strategy.update(item);
+      }
+    });
     return this.items;
   }
-
-  private updateItem(item: Item): void {
-    if (this.checkName(item, 'conjured')) {
-      this.updateConjuredItem(item);
-    } 
-    else {
-      if (item.name !== GildedRose.SULFURAS) {
-        item.sellIn -= 1;
-        }
-        if (this.checkName(item, GildedRose.AGED_BRIE)) {
-          item.quality = Math.min(item.quality + 1, GildedRose.MAX_QUALITY);
-          } 
-        else if (this.checkName(item, GildedRose.BACKSTAGE_PASSES)) {
-          if (item.sellIn < 0) {
-            item.quality = GildedRose.MIN_QUALITY;
-          } else {
-            item.quality = Math.min(item.quality + 1, GildedRose.MAX_QUALITY);
-          if (item.sellIn < 10) {
-            item.quality = Math.min(item.quality + 1, GildedRose.MAX_QUALITY);
-          }
-          if (item.sellIn < 5) {
-            item.quality = Math.min(item.quality + 1, GildedRose.MAX_QUALITY);
-          }
-        }
-      }
-      
-      else {
-        if (item.quality > GildedRose.MIN_QUALITY && item.name !== GildedRose.SULFURAS) {
-          item.quality -= 1;
-          if (item.sellIn < 0) {
-          item.quality -= 1;
-          }
-      }
-      
-    }
-    item.quality = Math.max(item.quality, GildedRose.MIN_QUALITY);
-    
-  }
-
 }
+
+  
+
+  
